53a5109223a55a71c54ee56a66903d0c1f55c9c5
Salwa Abo Oun <salwaoun8@gmail.com>
2021-02-21 10:56:09 +0400

    final

--- service-worker.js
+++ service-worker.js
@@ -16,14 +16,14 @@ self.addEventListener('install', (e) => {
     );
 });
 
-// self.addEventListener('fetch', function (e) {
-//     e.respondWith(// check if the cache has the file
-//         caches.match(e.request).then(function (r) {
-//             console.log('[Service Worker] Fetching resource: ' + e.request.url);// 'r' is the matching file if it exists in the cache
-//             return r
-//         })
-//     );
-// });
+self.addEventListener('fetch', function (e) {
+    e.respondWith(// check if the cache has the file
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);// 'r' is the matching file if it exists in the cache
+            return r
+        })
+    );
+});
 
 self.addEventListener('fetch', function (e) {
     e.respondWith(
